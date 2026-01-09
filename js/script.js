0636f1df66f53a25640742a7ad64ba26e5694708
Mohammad Fisal Rabin <fisal.rabin25@gmail.com>
2026-01-10 01:34:18 +0700

    update style and js

--- js/script.js
+++ js/script.js
@@ -1,6 +1,15 @@
 const hamburger = document.getElementById("hamburger");
 const navMenu = document.getElementById("nav-menu");
+const nav = document.querySelector("nav");
 
 hamburger.addEventListener("click", () => {
+    nav.classList.toggle("active");
     navMenu.classList.toggle("active");
+});
+
+document.querySelectorAll("#nav-menu a").forEach(link => {
+    link.addEventListener("click", () => {
+        nav.classList.remove("active");
+        navMenu.classList.remove("active");
+    });
 });
